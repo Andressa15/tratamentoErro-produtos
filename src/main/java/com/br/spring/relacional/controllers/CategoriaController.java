b2be482397d9a65be3e5d1a3863a55ba8f4b6263
Andress <andressaoliveiracintrad@gmail.com>
2019-08-13 23:29:32 -0300

    -

--- src/main/java/com/br/spring/relacional/controllers/CategoriaController.java
+++ src/main/java/com/br/spring/relacional/controllers/CategoriaController.java
@@ -6,48 +6,46 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.br.spring.relacional.models.Categoria;
+import com.br.spring.relacional.models.Login;
 import com.br.spring.relacional.services.CategoriaService;
 
 @Controller
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
-	@GetMapping("/categorias")
-	public ModelAndView exibirCategorias() {
-		ModelAndView modelAndView = new ModelAndView("categorias.html");
-		modelAndView.addObject("categorias", categoriaService.buscarTodasCategorias());
-		return modelAndView;
-	}
-	
+
 	@GetMapping("/cadastrar/categoria")
-	public ModelAndView exibirFormulario() {
-		ModelAndView modelAndView = new ModelAndView("cadastroCategoria.html");
-		return modelAndView;
-	}
-	
-	@PostMapping("/cadastrar/categoria")
-	public ModelAndView cadastrarCategoria(@Valid Categoria categoria, BindingResult bindingCategoria) {
-		ModelAndView modelAndView = new ModelAndView("cadastroCategoria.html");
-		
-		if(bindingCategoria.hasErrors()) {
-			List<String> mensagens = new ArrayList<String>();
-			for (ObjectError erro : bindingCategoria.getAllErrors()) {
-				mensagens.add(erro.getDefaultMessage());
-			}
-			modelAndView.addObject("mensagens", mensagens);
-		}else {
-			modelAndView.addObject("mensagens", categoriaService.cadastrarCategoria(categoria));
+	public ResponseEntity<?> exibirFormulario(@Valid @RequestBody Categoria categoria) {
+		try {
+			categoriaService.cadastrarCategoria(categoria);
+			return ResponseEntity.status(HttpStatus.CREATED).body(categoria);	
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
-		return modelAndView;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?>  categoriaId(@PathVariable int id) {
+		try {
+			categoriaService.buscarTodasCategorias();
+			return ResponseEntity.ok(id);
+		}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 }
